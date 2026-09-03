f9f11d8b972a8e6f7ae1cd297554660f94b5f157
kaisanwu <caisonnwu@gmail.com>
2026-09-03 14:59:51 +0800

    landing page 文案優化

--- js/flow.js
+++ js/flow.js
@@ -31,8 +31,8 @@ function stepA(){
       </video>
     </div>
     <div class="selpage-intro">
-      <h1>幫您檢視目前的閒置資金，找出更合適的運用方式。</h1>
-      <div class="lead">花 2 分鐘，讓「AI 智富管家」幫您盤點閒置資金，找出更適合的資金運用方式。</div>
+      <h1>幫您全面檢視資產配置，打造更佳的投資組合</h1>
+      <div class="lead">花 2 分鐘，讓「AI 智富管家」幫您盤點投資現況，提供合適的資產分析與佈局建議。</div>
     </div>
     <div id="startBtnMount" style="text-align:center;margin-top:var(--spacing-40)"></div>`;
   destroyActiveLottieIcons();screen().innerHTML='';screen().appendChild(p);
